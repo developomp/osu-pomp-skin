d1fe4facbb51bfb70332620a1371b3b2a061730f
developomp <developomp@gmail.com>
2022-02-16 22:27:43 +0900

    implemented hit burst logic

--- src/procedures/hit_bursts/setup.py
+++ src/procedures/hit_bursts/setup.py
@@ -1,6 +1,63 @@
+from PIL import Image
+from glob import glob
+
+from log import info
+
+
+def create_frames(image_path: str, create_k=False):
+    """Create frames for each hit burst"""
+
+    file_name = image_path.split("/")[-1]
+
+    # create hit100k too
+    if not create_k and "hit100" in file_name:
+        create_frames(image_path, True)
+
+    # create hit300 and hit300k
+    if "hit300" in file_name:
+        create_hit300s(image_path)
+        return
+
+    img = Image.open(image_path)
+
+    # i: 0 ~ 45
+    for i in range(46):
+        if create_k:
+            frame_file_path = f"{file_name.removesuffix('.png')}k-{i}@2x.png"
+        else:
+            frame_file_path = f"{file_name.removesuffix('.png')}-{i}@2x.png"
+
+        if i <= 30:
+            # no alterations for frame 0 ~ 30
+
+            img.save(f"./dist/{frame_file_path}")
+        else:
+            # linear fading for frame 31 ~ 45
+
+            transparent_img = img.convert("RGBA")
+            data = img.getdata()
+
+            new_data = []
+            for (r, g, b, a) in data:
+                a = int(a * (45 - i) / 14)
+                new_data.append((r, g, b, a))
+
+            transparent_img.putdata(new_data)  # type: ignore
+            transparent_img.save(f"./dist/{frame_file_path}")
+
+
+def create_hit300s(image_path: str):
+    img = Image.open(image_path)
+    file_name = image_path.split("/")[-1].removesuffix(".png")
+
+    img.save(f"./dist/{file_name}-0@2x.png")
+    img.save(f"./dist/{file_name}k-0@2x.png")
+
+
 #
 # main
 #
 
-# todo: generate hit0, hit100, hit100k, hit300, and hit300k
-# todo: animate fading
+
+for image_path in glob("src/procedures/hit_bursts/*.png"):
+    create_frames(image_path)
