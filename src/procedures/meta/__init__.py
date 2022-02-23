a1b68f8860a005bb86d6099353cf341a41f52a57
developomp <developomp@gmail.com>
2022-02-23 14:54:34 +0900

    fixed logic

--- src/procedures/meta/__init__.py
+++ src/procedures/meta/__init__.py
@@ -6,8 +6,8 @@ from shutil import copyfile
 #
 
 for file_path in glob("src/procedures/meta/*"):
-    if "/__init__.py" in file_path and "/__pycache__" in file_path:
+    if "/__init__.py" in file_path or "/__pycache__" in file_path:
         continue
-    
+
     file_name = file_path.split("/")[-1]
     copyfile(file_path, f"dist/{file_name}")
