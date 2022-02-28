794af8eacdaf3ff2e4d179d73a08831b991db358
developomp <developomp@gmail.com>
2022-02-28 19:04:15 +0900

    added opacity settings to followpoints

--- src/procedures/followpoints/__init__.py
+++ src/procedures/followpoints/__init__.py
@@ -1,4 +1,27 @@
-from helper import copy_all
+from PIL import Image
+from glob import glob
 
 
-copy_all("src/procedures/followpoints/*.png", add_postfix=True)
+#
+# main
+#
+
+opacity = 0.8
+
+# find all files
+for file_path in glob("src/procedures/followpoints/*.png"):
+    img = Image.open(file_path)
+    img = img.convert("RGBA")
+
+    # lower opacity
+
+    data = []
+    for (r, g, b, a) in img.getdata():
+        data.append((r, g, b, int(a * opacity)))
+
+    img.putdata(data)
+
+    # save image
+
+    file_name = file_path.split("/")[-1].removesuffix(".png")
+    img.save(f"dist/{file_name}@2x.png")
