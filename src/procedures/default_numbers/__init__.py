733aabb3ba1cee417362f30d60a3c7456ea0f6d2
developomp <developomp@gmail.com>
2023-01-21 15:17:32 +0900

    refactor: increase circle number size

--- src/procedures/default_numbers/__init__.py
+++ src/procedures/default_numbers/__init__.py
@@ -6,13 +6,13 @@ from PIL import ImageDraw
 # main
 #
 
-font = ImageFont.truetype("src/fonts/NotoSans-Regular.ttf", 80)
+font = ImageFont.truetype("src/fonts/NotoSans-Regular.ttf", 100)
 
 # i: 0 ~ 9
 for i in range(10):
-    image = Image.new("RGBA", (45, 100))
+    image = Image.new("RGBA", (50, 100))
     draw = ImageDraw.Draw(image)
 
-    draw.text((0, -7), f"{i}", fill="white", font=font)
+    draw.text((-1, -20), f"{i}", fill="white", font=font)
 
     image.save(f"dist/default-{i}@2x.png")
